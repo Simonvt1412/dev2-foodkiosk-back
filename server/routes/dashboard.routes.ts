92d15720ddc34acf5de7e943a95980e3606d5110
Simon Van Tomme <simon.vantomme@student.arteveldehs.be>
2025-06-13 17:38:03 +0200

    bijna

--- server/routes/dashboard.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getStats } from '../controllers/dashboard.controller';
-import { authenticateToken } from '../middleware/auth.middleware';
-
-const router = Router();
-
-router.get('/stats', authenticateToken, getStats);
-
-export default router;
